5793214bff6da65a8074891ef1a747dcc9002099
Ezequiel <ezemgaray@gmail.com>
2020-06-12 15:24:44 +0200

    Add function getQuestions

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,18 @@ var ws;
 
 // }
 var users = [];
+var currGame
 var user = JSON.parse(localStorage.getItem("user")) || {
-    id: "",
-    name: "",
-    image: "",
-    countGames: 0,
-    win: 0,
-    loose: 0,
-    currC: 0,
-    currW: 0,
-    currR: "",
-    ratio: 0 //(this.countGames == 0) ? 0 : ((this.win / this.countGames) * 100)
+   id: "",
+   name: "",
+   image: "",
+   countGames: 0,
+   win: 0,
+   loose: 0,
+   currC: 0,
+   currW: 0,
+   currR: "",
+   ratio: 0 //(this.countGames == 0) ? 0 : ((this.win / this.countGames) * 100)
 }
 
 console.log(user)
@@ -27,7 +28,7 @@ console.log(user)
 document.querySelector("#buttonId").addEventListener("click", onSendChat)
 
 function onSendChat() {
-    ws.send(`{"to":"quizGame", "user":${JSON.stringify(user)}, "content":"${document.querySelector("#Mensaje").value}", "type":"messageU"}`);
+   ws.send(`{"to":"quizGame", "user":${JSON.stringify(user)}, "content":"${document.querySelector("#Mensaje").value}", "type":"messageU"}`);
 }
 
 window.onbeforeunload = leaveGame;
@@ -41,71 +42,80 @@ init();
 
 
 function joinGame() {
-    ws = new WebSocket("wss://cloud.achex.ca");
-    ws.onopen = function (e) {
-        ws.send(`{"setID":"quizGame", "passwd":"12345"}`);
+   ws = new WebSocket("wss://cloud.achex.ca");
+   ws.onopen = function (e) {
+      ws.send(`{"setID":"quizGame", "passwd":"12345"}`);
 
-    }
-    ws.onmessage = function (response) {
-        let responseUser = JSON.parse(response.data);
+   }
+   ws.onmessage = function (response) {
+      let responseUser = JSON.parse(response.data);
 
-        if (responseUser.auth == "OK") {
-            ws.send(`{"to":"quizGame", "user":"${responseUser.SID}", "type":"connect"}`);
-            user.id = responseUser.SID;
-            localStorage.setItem("user", JSON.stringify(user))
-        }
+      if (responseUser.auth == "OK") {
+         ws.send(`{"to":"quizGame", "user":"${responseUser.SID}", "type":"connect"}`);
+         user.id = responseUser.SID;
+         localStorage.setItem("user", JSON.stringify(user))
+      }
 
-        switch (responseUser.type) {
-            case "connect":
-                sendUser(JSON.stringify(user))
-                break
-            case "messageU":
-                printMessage(responseUser.user, responseUser.content)
-                break
-            case "disconnect":
-                sendUser(JSON.stringify(user))
-                break
-            case "user":
-                printUsers(responseUser.user)
-                break
-        }
-    }
-    // ws.onclose = function (e) {
-    //     console.log("onclose")
-    // }
+      switch (responseUser.type) {
+         case "connect":
+            sendUser(JSON.stringify(user))
+            break
+         case "messageU":
+            printMessage(responseUser.user, responseUser.content)
+            break
+         case "disconnect":
+            sendUser(JSON.stringify(user))
+            break
+         case "user":
+            printUsers(responseUser.user)
+            break
+      }
+   }
+   // ws.onclose = function (e) {
+   //     console.log("onclose")
+   // }
 }
 
 function init() {
-    if (!user.name.length || !user.name) {
-        user.name = prompt("Name:");
-    }
-    joinGame();
+   if (!user.name.length || !user.name) {
+      user.name = prompt("Name:");
+   }
+   joinGame();
 }
 
 function sendUser(user) {
-    ws.send(`{"to":"quizGame", "user":${user}, "type":"user"}`);
-    users = [];
+   ws.send(`{"to":"quizGame", "user":${user}, "type":"user"}`);
+   users = [];
 }
 
 function printUsers(userData) {
-    if (userData.name != "" && userData.id != "") {
-        users.push({
-            name: userData.name,
-            userId: userData.id
-        });
-    }
-    console.log(users);
+   if (userData.name != "" && userData.id != "") {
+      users.push({
+         name: userData.name,
+         userId: userData.id
+      });
+   }
+   console.log(users);
 }
 
 function printMessage(user, message) {
-    var x = document.createElement("p");
-    x.textContent = user.name + ": " + message;
-    document.body.append(x);
+   var x = document.createElement("p");
+   x.textContent = user.name + ": " + message;
+   document.body.append(x);
 }
 
 function leaveGame() {
-    user.id = ""
-    user.name = ""
-    ws.send(`{"to":"quizGame", "userId":"", "username":"", "type":"disconnect"}`);
-    ws.close();
+   user.id = ""
+   user.name = ""
+   ws.send(`{"to":"quizGame", "userId":"", "username":"", "type":"disconnect"}`);
+   ws.close();
+}
+
+function getQuestions(amount = 10) {
+   axios
+      .get("https://opentdb.com/api.php?amount=" + amount)
+      .then(function (response) {
+         currGame = response.data.results
+         
+      })
 }
