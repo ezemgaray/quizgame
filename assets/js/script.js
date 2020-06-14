d58b2a3427ac72fc2455350a89e091ef6dc79816
Ezequiel <ezemgaray@gmail.com>
2020-06-14 03:22:11 +0200

    Update Questions - print and Add func mixAnswers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,9 @@
 var ws;
 var users = [];
 var currGame
+var answers
+let questionCount = 0 // cuenta ascendente de preguntas, al cargar preguntas que manejar el contador por el length del array de preguntas
+
 var globalInterval
 var user = JSON.parse(localStorage.getItem("user")) || {
    id: "",
@@ -267,13 +270,19 @@ function getQuestions(amount = 5) {
       })
 }
 
-function showQuestions() {
-   elem("#questions").classList.toggle("open")
-   let quest = elem("#questions")
-   quest.addEventListener("transitionend", showCountDown)
+function mixAnswers() {
+   answers = currGame[questionCount].incorrect_answers
+   answers.push(currGame[questionCount].correct_answer)
+   answers.sort(() => Math.random() - 0.5)
 }
 
-function showCountDown(e) {
+function showQuestions() {
+   elem("#questions").classList.toggle("open")
+   getQuestions()
+   elem("#questions").addEventListener("transitionend", showCountDown)
+}
+
+function showCountDown() {
    let countDown = document.createElement("div")
    countDown.className = "countdown"
    let countNumber = document.createElement("span")
@@ -301,30 +310,40 @@ function showCountDown(e) {
    elem("#questions").removeEventListener("transitionend", showCountDown)
 }
 
-let questionCount = 0 // cuenta ascendente de preguntas, al cargar preguntas que manejar el contador por el length del array de preguntas
 function showQuestion() {
-   questionCount++
-   if (questionCount > 2) {
+
+   if (questionCount + 1 > currGame.length) {
       elem("#questions").classList.toggle("open")
       elem("#question").remove()
       recuento()
       questionCount = 0
       return
    }
+   mixAnswers()
 
    if (elem("#question")) elem("#question").remove()
    elem("#questions").innerHTML = elem("#templateQuestion").innerHTML
-
+   //Insert question
+   elem("#question .question__category").innerText = currGame[questionCount].category
+   elem("#question .question__number").innerText = (questionCount + 1) + "/" + currGame.length
+   elem("#question h2").innerHTML = currGame[questionCount].question
    //Adaptar posibles respuestas en base a la api de preguntas.
+
    setTimeout(function () {
       elem("#question").classList.toggle("open")
       let buttons = elem(".answers button", true)
       let transition = 0.9
-      for (btn of buttons) {
-         btn.style.animation = `appear ${transition += 0.2}s ease-in-out forwards`
-      }
+      answers.forEach((btn, index) => {
+         let button = document.createElement("button")
+         button.className = "question__btn btn-grad"
+         button.dataset.answers = index
+         button.innerHTML = btn
+         elem("#question .answers").append(button)
+         button.style.animation = `appear ${transition += 0.2}s ease-in-out forwards`
+      })
       questionTime()
    }, 200)
+   questionCount++
 }
 
 function questionTime() {
